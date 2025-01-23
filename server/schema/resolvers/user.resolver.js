c90d77a38afe5fbffd7e11e744a10db632a98625
badAl <badalsingh120403@gmail.com>
2025-01-23 20:44:39 +0530

    feat: create signUp mutation

--- server/schema/resolvers/user.resolver.js
+++ server/schema/resolvers/user.resolver.js
@@ -1,6 +1,50 @@
+import bcrypt from 'bcryptjs';
 import { users } from '../../data/index.js';
+import User from '../../models/user.model.js';
 
 const userResolver = {
+    Mutation: {
+        signUp: async (_, { input }, context) => {
+            try {
+                const { username, name, password, gender } = input;
+
+                if (!username || !name || !password || !gender) {
+                    throw new Error('All fields are required');
+                }
+
+                const exitingUser = await User.findOne({ username });
+
+                if (exitingUser) {
+                    throw new Error('User already exists');
+                }
+
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
+
+                const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+                const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+                const newUser = new User({
+                    username,
+                    name,
+                    password: hashedPassword,
+                    gender,
+                    profilePicture:
+                        gender === 'male' ? boyProfilePic : girlProfilePic,
+                });
+
+                await newUser.save();
+
+                await context.login(newUser);
+
+                return newUser;
+            } catch (error) {
+                console.error('Error in SignUp: ', error);
+                throw new Error(error.message || 'Internal Server Error');
+            }
+        },
+    },
+
     Query: {
         users: () => {
             return users;
@@ -10,8 +54,6 @@ const userResolver = {
             return users.find((user) => user._id === userId);
         },
     },
-
-    Mutation: {},
 };
 
 export default userResolver;
